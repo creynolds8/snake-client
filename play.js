df2c73e4ccd987ba95bf2ef6066153b55acc0e8c
creynolds8 <cameron8.cr@gmail.com>
2024-05-08 15:10:55 -0700

    remove connect function and require client

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
 const net = require('net');
+const connect = require('client')
 
-const connect = function() {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
 
-  conn.on('connect', () => {
-    console.log('Connected!');
-  });
-  conn.on('data', data => {
-    console.log(data);
-  })
-  conn.setEncoding('utf8');
-  return conn;
-};
 
 console.log('Connecting...');
 connect();
