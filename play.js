3a811cf14e3e7220746b50b43a5eddb8a070273d
creynolds8 <cameron8.cr@gmail.com>
2024-05-08 18:05:47 -0700

    remove last two functions to match what file should be

--- play.js
+++ play.js
@@ -1,40 +1,6 @@
-const net = require('net');
-const connect = require('./client');
-const readline = require('readline');
-//const input = require('./input');
-
-// const rl = readline.write({
-//   input: process.input,
-//   output: process.input,
-// })
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') process.exit()
-  // switch ('data') {
-  //   case '\u0003':
-  //     process.exit()
-  //   case 'w':
-  //     conn.write('Move: up')
-  //   case 's':
-  //     conn.write('Move: down')
-  //   case 'a':
-  //     conn.write('Move: left')
-  //   case 'd':
-  //     conn.write('Move: right')
-  //}
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', handleUserInput)
-  return stdin;
-};
+const { connect }= require('./client');
+const { setUpInput }= require('./input');
 
 console.log('Connecting...');
 connect();
-setupInput();
-
-module.exports = 'data';
+setUpInput();
