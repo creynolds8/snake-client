17617eef60211d20f0c77807c7c60f9855b5d78f
creynolds8 <cameron8.cr@gmail.com>
2024-05-08 17:10:38 -0700

    add function to listen for user input

--- play.js
+++ play.js
@@ -1,6 +1,14 @@
 const net = require('net');
 const connect = require('./client')
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput)
+  return stdin;
+};
 
 console.log('Connecting...');
 connect();
